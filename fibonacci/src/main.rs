b7c39dc4f5920eb393ae02553ca408a18cf48fb2
bolusarz <bolusarz@gmail.com>
2026-07-12 20:43:00 +0100

    Implement fibonacci sequence generator with user input validation

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,58 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("🔢 Fibonacci Sequence Generator");
+    println!("Enter the number of terms you want to generate:");
+
+    let num_terms = match get_input_as_u32() {
+        Some(num) => num,
+        None => {
+            println!("❌ Invalid input. Pleas enter a positive integer");
+
+            return;
+        }
+    };
+
+    if num_terms == 0 {
+        println!("❌ Number of terms must be greater than zero.");
+
+        return;
+    }
+
+    let sequence = generate_fibonacci(num_terms);
+    println!(
+        "✅ Fibonacci sequence ({} terms): {:?}",
+        num_terms, sequence
+    )
+}
+
+fn get_input_as_u32() -> Option<u32> {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("❌ Failed to read input.");
+
+    match input.trim().parse::<u32>() {
+        Ok(num) => Some(num),
+        Err(_) => None,
+    }
+}
+
+fn generate_fibonacci(n: u32) -> Vec<u64> {
+    let mut sequence = Vec::new();
+
+    if n >= 1 {
+        sequence.push(0);
+    }
+
+    if n >= 2 {
+        sequence.push(1);
+    }
+
+    for i in 2..n {
+        let next = sequence[i as usize - 1] + sequence[i as usize - 2];
+        sequence.push(next);
+    }
+
+    sequence
 }
